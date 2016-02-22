9db9ccd394d9ee12005f6718fec2bb9202918293
ronin <roninliu@foxmail.com>
2016-02-22 18:02:58 +0800

    demo

--- www/live.js
+++ www/live.js
@@ -4,11 +4,11 @@ var path = require('path');
 var rootPath = path.dirname(__dirname);
 
 var instance = new thinkjs({
-  APP_PATH: rootPath + path.sep + 'app',
-  RUNTIME_PATH: rootPath + path.sep + 'runtime',
-  ROOT_PATH: rootPath,
-  RESOURCE_PATH: __dirname,
-  env: 'production'
+	APP_PATH: rootPath + path.sep + 'app',
+	RUNTIME_PATH: rootPath + path.sep + 'runtime',
+	ROOT_PATH: rootPath,
+	RESOURCE_PATH: __dirname,
+	env: 'live'
 });
 
 //preload packages before start server.
